8a7ce62bf3c7b9cda582bad75bc91024eb2de12f
Oden <ops991923@gmail.com>
2015-05-17 13:28:39 -0700

    add substring search

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,8 +4,13 @@ source ~/.dotfiles/antigen/antigen.zsh
 # plugins
 antigen use oh-my-zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle git
 antigen bundle npm
+
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
 # theme
 antigen bundle sindresorhus/pure
