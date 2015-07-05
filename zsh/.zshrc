7349493536cdab88e989c53c52abd11d034c250a
Oden <ops991923@gmail.com>
2015-07-04 17:59:52 -0700

    remove pure, git & npm aliases, add super simple prompt

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,4 +3,16 @@ do
 	source $file
 done
 
+# prompt
+[[ "$SSH_CONNECTION" != "" ]] && in_ssh="!"
+PROMPT="%{$fg[blue]%}${PWD/$HOME/~}%{$reset_color%}
+%{$fg[red]%}$in_ssh%{$reset_color%}— "
+
+# update prompt after executing commands
+precmd()
+{
+	PROMPT="%{$fg[blue]%}${PWD/$HOME/~}%{$reset_color%}
+%{$fg[red]%}$in_ssh%{$reset_color%}— "
+}
+
 # so nvm doesn't append here: nvm.sh
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,16 +5,10 @@ source ~/.dotfiles/antigen/antigen.zsh
 antigen use oh-my-zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
-antigen bundle git
-antigen bundle npm
 
 zmodload zsh/terminfo
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
-
-# theme
-antigen bundle mafredri/zsh-async
-antigen bundle sindresorhus/pure
 
 # apply plugins
 antigen apply
