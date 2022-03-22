f571345036a60d8180d7c63d1ed5d6012f2e0102
Oden <hello@odensc.me>
2022-03-21 22:47:06 -0700

    fix substring search keybinds

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -3,5 +3,7 @@ bindkey "^[[1;5D" backward-word
 bindkey "^[[1;5C" forward-word
 
 # bind up/down to substring search
-bindkey "^[[B" history-substring-search-down
-bindkey "^[[A" history-substring-search-up
+bindkey '^[[A' history-substring-search-up
+bindkey '^[OA' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^[OB' history-substring-search-down
